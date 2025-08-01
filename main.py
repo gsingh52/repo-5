2c2d365f761447835d31164abd11bcb6da4651d2
gsingh52 <gds_52@yahoo.com>
2025-08-01 17:06:38 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to Python Flask World version v:1.0 from CICD GKE'
+
+if __name__  == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+ 
+
